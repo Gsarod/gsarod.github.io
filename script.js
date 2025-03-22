3ae3cb8eeed9334da1d564dd6082f8d7fe7c7ce3
Gsarod <ungurasu.dev@gmail.com>
2025-03-22 18:38:00 +0100

    Jarvis '95 the fuck out of it

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
     
-    // Fetches the description parmeters
-    const buttons = document.querySelectorAll('btn');       
-    const infoBlock = document.getElementById('infoBlock'); 
-    const infoTitle = document.getElementById('infoTitle'); 
-    const infoTag = document.getElementById('infoTag');
-    const infoBar = document.getElementById('infoBar');
-    const infoBox = document.getElementById('infoBox');
-
-
+    // Fetches the description parameters
+    const [buttons, infoBlock, infoTitle, infoTag, infoBox, StartBar, BodyBar, EndBar] = [
+        document.querySelectorAll('btn'),
+        ...['infoBlock', 'infoTitle', 'infoTag', 'infoBox', 'infoBar', 'BarStart', 'BarBody', 'BarEnd']
+            .map(id => document.getElementById(id))
+    ];
 
     buttons.forEach(button => {
         button.addEventListener('mouseover', () => {
-            infoTitle.textContent = button.getAttribute('title');
-            infoTag.textContent = button.getAttribute('tag');
-            infoBlock.textContent = button.getAttribute('desc');
-    
-            // Initalises the descriptions upon hovering on the button
-            infoTitle.style.display = 'block';
-            infoTag.style.display = 'block';
-            infoBlock.style.display = 'block';
-            infoBar.style.display = 'block';
-            infoBox.style.display = 'block';
+            // Initializes the descriptions upon hovering on the button
+            [infoTitle, infoTag, infoBlock].forEach((el, idx) => el.textContent = button.getAttribute(['title', 'tag', 'desc'][idx]));
+            [infoTitle, infoTag, infoBlock, infoBox, StartBar, BodyBar, EndBar].forEach(el => el.style.display = 'block');
         });
 
         button.addEventListener('mouseleave', () => {
-            infoBlock.style.display = 'none';
-            infoTitle.style.display = 'none';
-            infoTag.style.display = 'none';
-            infoBar.style.display = 'none';
-            infoBox.style.display = 'none';
+            // Hides all the description elements when the mouse leaves
+            [infoTitle, infoTag, infoBlock, infoBox, StartBar, BodyBar, EndBar].forEach(el => el.style.display = 'none');
         });
     });
+    
+    
+    
+    let time = document.getElementById('time');
+    setInterval(() =>{
+        let d = new Date();
+        time.innerHTML = d.toLocaleTimeString();
+    },1000)
+
 });
+
+
