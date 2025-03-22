7d39e1557bff2b967e277c3586bd51031ab3b27a
Gsarod <ungurasu.dev@gmail.com>
2025-03-22 01:25:10 +0100

    Smol  update

--- script.js
+++ script.js
@@ -5,17 +5,31 @@ document.addEventListener("DOMContentLoaded", function () {
     const infoBlock = document.getElementById('infoBlock'); 
     const infoTitle = document.getElementById('infoTitle'); 
     const infoTag = document.getElementById('infoTag');
+    const infoBar = document.getElementById('infoBar');
+    const infoBox = document.getElementById('infoBox');
+
+
 
     buttons.forEach(button => {
         button.addEventListener('mouseover', () => {
-            infoTitle.textContent = button.getAttribute('data-title');
-            infoTag.textContent = button.getAttribute('data-tag');
-            infoBlock.textContent = button.getAttribute('data-info');
+            infoTitle.textContent = button.getAttribute('title');
+            infoTag.textContent = button.getAttribute('tag');
+            infoBlock.textContent = button.getAttribute('desc');
     
             // Initalises the descriptions upon hovering on the button
             infoTitle.style.display = 'block';
             infoTag.style.display = 'block';
             infoBlock.style.display = 'block';
+            infoBar.style.display = 'block';
+            infoBox.style.display = 'block';
+        });
+
+        button.addEventListener('mouseleave', () => {
+            infoBlock.style.display = 'none';
+            infoTitle.style.display = 'none';
+            infoTag.style.display = 'none';
+            infoBar.style.display = 'none';
+            infoBox.style.display = 'none';
         });
     });
 });
